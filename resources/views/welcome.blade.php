fb9227b632b8d1e1376628221a2eb99958c6f694
Mandy Lin <smilemaple200901@gmail.com>
2019-01-15 20:52:38 +0800

    modified index page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,8 +65,15 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
+            
+
+            <div class="content">
+                <div class="title m-b-md">
+                    {{ config('app.name') }}
+                </div>
+
+                @if (Route::has('login'))
+                <div class="links">
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
@@ -77,21 +84,7 @@
                         @endif
                     @endauth
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    {{ config('app.name') }}
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                @endif
             </div>
         </div>
     </body>
